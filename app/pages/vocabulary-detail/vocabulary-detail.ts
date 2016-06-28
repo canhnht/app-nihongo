87be746801b2e7c998fd50c6fc7ee8e308dded19
canhnht <canhnht1709@gmail.com>
2016-06-28 16:36:52 +0800

    Update list item

---
+++ app/pages/vocabulary-detail/vocabulary-detail.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+/*
+  Generated class for the VocabularyDetailPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/vocabulary-detail/vocabulary-detail.html',
+})
+export class VocabularyDetailPage {
+  constructor(public nav: NavController) {}
+}
