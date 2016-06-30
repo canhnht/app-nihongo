291c1c22235cbec0a4917fd5da45aa4801fac83e
canhnht <canhnht1709@gmail.com>
2016-06-30 16:04:21 +0800

    Remove navigation button in slides

--- app/pages/vocabulary-slides/vocabulary-slides.ts
+++ app/pages/vocabulary-slides/vocabulary-slides.ts
@@ -1,6 +1,7 @@
-import {Component, ViewChild, ApplicationRef} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NavController, NavParams, Slides} from 'ionic-angular';
 import {BottomAudioController} from '../../components/bottom-audio-controller/bottom-audio-controller';
+import {AudioService} from '../../providers/audio.service';
 
 @Component({
   templateUrl: 'build/pages/vocabulary-slides/vocabulary-slides.html',
@@ -14,7 +15,6 @@ export class VocabularySlides {
   };
   title: String = 'Mimi Kara Nihongo';
   slides: any[] = [];
-  selectedSlide: number = 1;
   direction: number = 0;
   head: number;
   tail: number;
@@ -22,12 +22,10 @@ export class VocabularySlides {
   default_slides: any[];
   firstChange: boolean = false;
 
-  constructor(private _navController: NavController, private navParams: NavParams,
-    private appRef: ApplicationRef) {
+  constructor(private _navController: NavController, private navParams: NavParams) {
     this.title = this.navParams.data.title;
 
     var getColor = function ( nr ) {
-      console.log('getColor', nr);
       return nr % 2 === 0 ? '#8080c5' : '#80b280';
     };
 
@@ -46,7 +44,6 @@ export class VocabularySlides {
     this.slides = [...this.default_slides];
     this.head = this.slides[0].nr;
     this.tail = this.slides[this.slides.length - 1].nr;
-    console.log('check', this.head, this.tail);
   }
 
   makeSlide( nr, data ) {
@@ -65,13 +62,11 @@ export class VocabularySlides {
 
   onSlideChanged($event) {
     if (!this.firstChange) return this.firstChange = true;
-    // console.log($event.activeIndex);
     let i = -1;
     let activeIndex = $event.activeIndex;
     if (activeIndex == 1 || activeIndex == 4) i = 0;
     else if (activeIndex == 2) i = 1;
     else i = 2;
-    // console.log('iiiiiii', i);
 
     let previous_index = (i == 0 ? 2 : i - 1);
     let next_index = (i == 2 ? 0 : i + 1);
@@ -79,9 +74,6 @@ export class VocabularySlides {
     this.slides[new_direction > 0 ? next_index : previous_index] =
       this.createSlideData(new_direction, this.direction);
     this.direction = new_direction;
-    setTimeout(() => {
-      this.appRef.tick();
-    }, 500);
   }
 
   createSlideData(new_direction, old_direction) {
@@ -91,44 +83,15 @@ export class VocabularySlides {
     } else {
       this.head = old_direction > 0 ? this.tail - 3 : this.head - 1;
     }
-    console.log(this.head, this.tail);
     nr = (new_direction == 1 ? this.tail : this.head);
     if (this.default_slides_indexes.indexOf(nr) !== -1) {
       return this.default_slides[this.default_slides_indexes.indexOf(nr)];
     }
     var getColor = function ( nr ) {
-      console.log('getColor', nr);
       return nr % 2 === 0 ? '#8080c5' : '#80b280';
     };
     return this.makeSlide( nr, {
       title : 'generated slide', color:  getColor( nr ) }
     );
   }
-
-  // onSlideChanged($event) {
-  //   const TOTAL_VOCABULARIES = 10;
-  //   let currentIndex = -1;
-  //   let activeIndex = $event.activeIndex;
-  //   if (activeIndex == 1 || activeIndex == 4) {
-  //     currentIndex = 0;
-  //   } else if (activeIndex == 2) {
-  //     currentIndex = 1;
-  //   } else if (activeIndex == 0 || activeIndex == 3) {
-  //     currentIndex = 2;
-  //   }
-  //   let prevIndex = (currentIndex - 1 + 3) % 3;
-  //   let nextIndex = (currentIndex + 1) % 3;
-  //   console.group('test');
-  //   console.log(prevIndex, currentIndex, nextIndex);
-  //   console.log(this.slides[prevIndex].vocabIndex,
-  //     this.slides[currentIndex].vocabIndex,
-  //     this.slides[nextIndex].vocabIndex);
-  //   console.groupEnd();
-
-  //   this.slides[prevIndex].vocabIndex =
-  //     (this.slides[currentIndex].vocabIndex - 1 + TOTAL_VOCABULARIES) % TOTAL_VOCABULARIES;
-  //   this.slides[nextIndex].vocabIndex =
-  //     (this.slides[currentIndex].vocabIndex + 1) % TOTAL_VOCABULARIES;
-  // }
-
 }
