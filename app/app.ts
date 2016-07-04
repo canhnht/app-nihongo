f4ea0374fd5082d04b7ea2692bf159d353324841
canhnht <canhnht1709@gmail.com>
2016-07-04 22:21:46 +0800

    Fix app

--- app/app.ts
+++ app/app.ts
@@ -18,7 +18,7 @@ export class MyApp {
   private rootPage:any;
 
   constructor(private platform:Platform) {
-    this.rootPage = VocabularySlides;
+    this.rootPage = HomePage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
