c3275e6d36abacd8ab474ae35c9965278376968e
canhnht <canhnht1709@gmail.com>
2017-01-16 22:16:51 +0700

    Fix reset setting in units

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ let firebase = require('firebase');
 })
 export class MyApp {
   @ViewChild('content') nav: NavController;
-  rootPage = PlaygroundPage;
+  rootPage = HomePage;
   // rootPage = HomeTmpPage;
   homePage = HomePage;
   playlistsPage = PlaylistsPage;
